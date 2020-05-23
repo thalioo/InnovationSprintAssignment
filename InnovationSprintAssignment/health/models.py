a76acbab26d8bb5ab5927231b7c1c15f85c1f0d5
thalioo <t.diniaco@gmail.com>
2020-05-23 11:51:44 +0300

    Changed Active Status field to meet requirements

--- InnovationSprintAssignment/health/models.py
+++ InnovationSprintAssignment/health/models.py
@@ -1,27 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-# class UserProfile(models.Model):
-# 	#Extend Djangos Built in User Model
-# 	user = models.OneToOneField(User,on_delete=models.CASCADE)
-# 	# The additional attributes we wish to include.
-# 	website = models.URLField(blank=True)
 
-# 	# Override the __unicode__() method to return out something meaningful!
-# 	# Remember if you use Python 2.7.x, define __unicode__ too!
-# 	def __str__(self):
-# 		return self.user.username
-# 	def __str__(self): # For Python 2, use __unicode__ too
-# 		return self.title
 class UserTemps(models.Model):
 	#Define a many to one relationship with the UserProfile Model
 	userId = models.ForeignKey(User, on_delete=models.CASCADE)
 	temperature = models.FloatField(default=36.6,
         validators=[MaxValueValidator(42), MinValueValidator(35)])
 	timeStamp = models.DateTimeField('date-time of temperature')
-	active = models.BooleanField(default=False)
+	HEALTHY = 'HEALTHY'
+	FEVER = 'ONGOINGFEVER'
+	STATUS = [ ('HEALTHY',_('Healthy')),
+			('FEVER',_('ONGOINGFEVER')),
+		]
+	active_status = models.CharField(
+	    max_length=8,
+	    choices=STATUS,
+	    default=HEALTHY,
+	)
 	class Meta:
 		#Creates mutliple key
 		unique_together = (("userId","timeStamp"))
