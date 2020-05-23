0a67c9690e3ca31377321393d76970deb05d849a
thalioo <t.diniaco@gmail.com>
2020-05-23 11:22:24 +0300

    Altered User Table to Djangos Default

--- InnovationSprintAssignment/health/models.py
+++ InnovationSprintAssignment/health/models.py
@@ -3,32 +3,35 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
-class UserProfile(models.Model):
-	#Extend Djangos Built in User Model
-	user = models.OneToOneField(User,on_delete=models.CASCADE)
-	# The additional attributes we wish to include.
-	website = models.URLField(blank=True)
+# class UserProfile(models.Model):
+# 	#Extend Djangos Built in User Model
+# 	user = models.OneToOneField(User,on_delete=models.CASCADE)
+# 	# The additional attributes we wish to include.
+# 	website = models.URLField(blank=True)
 
-	# Override the __unicode__() method to return out something meaningful!
-	# Remember if you use Python 2.7.x, define __unicode__ too!
-	def __str__(self):
-		return self.user.username
-	def __str__(self): # For Python 2, use __unicode__ too
-		return self.title
+# 	# Override the __unicode__() method to return out something meaningful!
+# 	# Remember if you use Python 2.7.x, define __unicode__ too!
+# 	def __str__(self):
+# 		return self.user.username
+# 	def __str__(self): # For Python 2, use __unicode__ too
+# 		return self.title
 class UserTemps(models.Model):
 	#Define a many to one relationship with the UserProfile Model
-	userId = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	userId = models.ForeignKey(User, on_delete=models.CASCADE)
 	temperature = models.FloatField(default=36.6,
         validators=[MaxValueValidator(42), MinValueValidator(35)])
 	timeStamp = models.DateTimeField('date-time of temperature')
-	active = models.DateTimeField()
+	active = models.BooleanField(default=False)
 	class Meta:
+		#Creates mutliple key
 		unique_together = (("userId","timeStamp"))
 
 class UserFeverSessions(models.Model):
-	id = models.AutoField(primary_key=True)
-	userId = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	# id = models.AutoField(User=True)
+	userId = models.ForeignKey(User, on_delete=models.CASCADE)
 	startTime = models.DateTimeField('start date-time of fever session')
 	endTime = models.DateField('end date-time of fever session')
+	def getActiveSession(self,date1,date2):
+		return self.startTime>=date1,self.endTime<=date2
 	
 		
